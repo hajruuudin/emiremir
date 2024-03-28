dc24117f3a6af343ac7d50f4965f2850ffa5a6db
hajruuudin <hajruuudin@gmail.com>
2024-03-28 21:10:04 +0100

    webpage

---
+++ assets/js/spapp/script.js
@@ -0,0 +1,6 @@
+let app = $.spapp({
+    defaultView: "#hajrudinexample", //Defaul view that loads when u open the webpage
+    templateDir: "./views/" //This is where the data-load attribute searches for the files
+});
+
+app.run();
